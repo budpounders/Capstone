9893a8d5f306ffe3138860e3916451a0272ca86d
Dan <it@4momsonline.com>
2009-10-18 23:30:48 -0400

    Started JS Clock

---
+++ public/javascripts/clock.js
@@ -0,0 +1,68 @@
+/*
+ Modify clock to fit your needs
+*/
+
+
+var ChessClock = function (player) {
+	this.player = player;
+	this.seconds = 0;
+	this.on = 0;
+
+	this.start = function () {
+		this.on = 1;
+		document.getElementById(this.player+'_td').style.backgroundColor = 'green';
+	};
+
+	this.stop = function () {
+		this.on = 0;
+		document.getElementById(this.player+'_td').style.backgroundColor = 'white';
+	}
+
+}
+
+ChessClock.prototype.tick = function () {
+	if (this.on) {
+		var i = document.getElementById(this.player);
+		i.value = toTime(++this.seconds);	
+	}
+};
+
+var timeout;
+var player1 = new ChessClock('clock1');
+
+function tick() {
+	clearTimeout(timeout);
+	timeout = setTimeout(tick, 1000);
+
+	player1.tick();
+}
+
+function start() {
+
+	if (!player1.on) {
+		player1.start();
+	} else {
+		player2.start();
+	}
+
+	tick();
+}
+
+function reset() {
+	clearTimeout(timeout);
+	player1.stop();
+	player1.seconds = 0;
+
+	document.getElementById('clock1').value=0;
+}
+
+
+function toTime (secs) {
+	//minutes
+	var hours = Math.floor(secs/3600);
+	var min = Math.floor((secs-hours*3600)/60);
+	var secs = secs - (min*60);
+
+	return hours + (min<10?':0':':')+min +(secs<10?':0':':') + secs;
+}
+
